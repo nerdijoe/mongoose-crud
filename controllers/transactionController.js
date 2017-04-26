dced7fadb4ee5886efe7559f2f4dbe3385caa114
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-04-26 13:39:37 +0700

    managed to create new transaction

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,6 +1,29 @@
 var Transaction = require("../models/transaction");
 
 exports.create = (req, res, next) => {
+
+/*
+
+insert raw (select json) in postman
+{
+  "memberid": "thebeard",
+  "days": 6,
+  "out_date": "2017/04/26",
+  "due_date": "2017/04/26",
+  "in_date": "2017/04/26",
+  "fine": 2000,
+  "booklist":
+    [
+      "58ff3521a7e3d7184cd8d2fd",
+      "58ff358ef4b47f185de3dd2f"
+    ]
+}
+
+*/
+
+  console.log(req.body);
+  // console.log(JSON.parse(req.body));
+
   var newTransaction = Transaction({
     memberid: req.body.memberid,
     days: req.body.days,
@@ -8,6 +31,22 @@ exports.create = (req, res, next) => {
     due_date: req.body.due_date,
     in_date: req.body.in_date,
     fine: req.body.fine,
+    booklist: req.body.booklist
+  });
 
+  newTransaction.save( (err, transaction) => {
+    if (err) res.send(err);
+
+    res.send(transaction);
+  })
+
+
+}
+
+exports.get_transactions = (req, res, next) => {
+  Transaction.find( (err, transactions) => {
+    if(err) res.send(err);
+
+    res.send(transactions);
   })
 }
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,2 +1,12 @@
+var express = require('express');
 var router = express.Router();
 var transaction_controller = require("../controllers/transactionController");
+
+
+// create transaction
+router.post('/', transaction_controller.create);
+
+// get all transactions
+router.get('/', transaction_controller.get_transactions);
+
+module.exports = router;
